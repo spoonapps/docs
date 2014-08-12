56a090c712f23cb1c3558b6676070ea1f32b51eb
Matt Black <matt@spoon.net>
2014-08-12 12:44:31 -0700

    add build.py, update .gitignore and README

--- README.md
+++ README.md
@@ -4,3 +4,21 @@ This is the official Git repository for Spoonium's public documentation.
 
 Live version of the docs can be found at http://spoonium.net/docs
 
+## Building the Docs ##
+
+### What You'll Need ###
+
+- Python 2.7
+- Pip
+
+Python dependencies are listed in the py-reqs.txt file in the root directory of this repo. 
+
+To install these dependencies, run: 
+	`pip install -r py-reqs.txt` 
+
+### Building ###
+
+Execute: `python build.py` from the root directory of the repo. 
+
+This will generate a "build" folder with the same structure as the "docs" folder, but with each .md file converted to and saved as an HTML file. 
+
---
+++ build.py
@@ -0,0 +1,64 @@
+import sys,os
+import re
+from markdown2 import markdown_path
+
+def main(root_dir):
+	"""Do the actual work of the script
+	"""
+	doc_dir = os.path.join(root_dir, "doc")
+	md_files = get_md_files(doc_dir)
+	#create the build directory(s) if it doesn't already exist
+	build_dir = os.path.join(root_dir, "build")
+	if not os.path.exists(build_dir):
+		os.makedirs(build_dir)
+	#convert the md files to html and write them to a file
+	for md in md_files:
+		file_name = os.path.basename(md)[:-3] + ".html"
+		html = convert_to_html(md)
+		sp_html = convert_html_to_spooniumHTML(html)
+		#form the filepath for the new file
+		output_dir = os.path.dirname(md).replace("\\doc\\", "\\build\\")
+		#make sure the directory exists
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir)
+		#write it to a file
+		write_to_file(file_path=os.path.join(output_dir, file_name), text=sp_html)
+		
+def get_md_files(root_dir):
+	"""Traverses all of the subdirectories in the supplied directory
+	and returns a list of the full paths to each markdown file
+	in the directory
+	"""
+	return [os.path.join(d, f)
+			for d, sub_dir, files in os.walk(root_dir)
+			for f in files if f.endswith(".md")]
+
+def convert_to_html(md_file_path):
+	"""Converts the markdown file to the expected HTML format to 
+	be used in the spoonium docs
+	"""
+	return markdown_path(md_file_path)
+
+def convert_html_to_spooniumHTML(html_in):
+	"""Wraps the input html in a <div class="wiki-content"></div>
+	for use in the spoonium docs
+	"""
+	return '<div class="wiki-content">\n' + html_in + '\n</div>'
+
+def write_to_file(file_path, text):
+	"""Write the contents of the input text to a file
+	at the given file_path
+	"""
+	f = open(file_path, 'w')  #overwrite if the file already exists
+	f.write(text)
+	f.close()
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		print("no args necessary -- run from the root directory of the repo!")
+		sys.exit(1)
+	else:
+		#default to 
+		script_dir = os.path.dirname(os.path.realpath(__file__))
+		main(script_dir)
+		sys.exit(0)
